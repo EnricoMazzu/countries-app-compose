906c705f2126a2363a684f49e640e6c750102c5f
Enrico <enrico.mazzucchelli@vipera.com>
2022-08-18 09:58:15 +0200

    minor

--- app/src/main/java/com/fabrick/lab/demo/compose/countriesapp/ui/screen/countries/CountriesScreen.kt
+++ app/src/main/java/com/fabrick/lab/demo/compose/countriesapp/ui/screen/countries/CountriesScreen.kt
@@ -1,6 +1,10 @@
 package com.fabrick.lab.demo.compose.countriesapp.ui.screen.countries
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
@@ -8,13 +12,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.fabrick.lab.demo.compose.countriesapp.common.Resource
 import com.fabrick.lab.demo.compose.countriesapp.data.impl.mock.MockData
 import com.fabrick.lab.demo.compose.countriesapp.domain.model.Country
 import com.fabrick.lab.demo.compose.countriesapp.ui.AppBarState
 import com.fabrick.lab.demo.compose.countriesapp.ui.navigation.NavMenuAction
 import com.fabrick.lab.demo.compose.countriesapp.ui.navigation.addNavMenuActions
-import com.fabrick.lab.demo.compose.countriesapp.ui.rememberAppBarState
 import com.fabrick.lab.demo.compose.countriesapp.ui.theme.CountriesAppTheme
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.SwipeRefreshState
@@ -52,6 +54,7 @@ fun CountriesScreen(
     CountriesScreenContent(
         countries = uiState.value.countries,
         refreshState = refreshState,
+        showProgress = uiState.value.onLoading,
         onCountrySelected = onDetailsRequired,
         onReloadRequired = {
             viewModel.reload();
@@ -63,23 +66,33 @@ fun CountriesScreen(
 fun CountriesScreenContent(
     modifier: Modifier = Modifier,
     countries: List<Country> = Collections.emptyList(),
+    showProgress: Boolean,
     refreshState: SwipeRefreshState,
     onCountrySelected: (Country) -> Unit = {},
-    onReloadRequired: () -> Unit = {}
+    onReloadRequired: () -> Unit = {},
 ) {
-    SwipeRefresh(
-        state = refreshState,
-        onRefresh = {
-            Timber.d("Refreshing....")
-            onReloadRequired()
+    // use box to allows overlapping. In case
+    Box(modifier = modifier.fillMaxSize()) {
+        AnimatedVisibility(visible = showProgress) {
+            LinearProgressIndicator(
+                Modifier.fillMaxWidth()
+            )
         }
-    ){
-        CountriesList(
-            modifier = modifier,
-            itemsToDraw = countries,
-            onCountrySelected = onCountrySelected
-        )
+        SwipeRefresh(
+            state = refreshState,
+            onRefresh = {
+                Timber.d("Refreshing....")
+                onReloadRequired()
+            }
+        ){
+            CountriesList(
+                modifier = modifier,
+                itemsToDraw = countries,
+                onCountrySelected = onCountrySelected
+            )
+        }
     }
+
 
 }
 
@@ -90,7 +103,6 @@ fun CountryScreenContentPreview(){
     CountriesAppTheme {
         Surface(Modifier.fillMaxSize()) {
             CountriesScreenContent(
-                refreshState = rememberSwipeRefreshState(isRefreshing = false),
                 countries = MockData.getCountries().map {
                     Country(
                         code = it.code,
@@ -98,7 +110,9 @@ fun CountryScreenContentPreview(){
                         emoji = "\uD83C\uDDE6\uD83C\uDDE9",
                         languages = Collections.emptyList()
                     )
-                }
+                },
+                refreshState = rememberSwipeRefreshState(isRefreshing = false),
+                showProgress = false
             )
         }
     }
